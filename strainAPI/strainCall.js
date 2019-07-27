f2937a9795f3af5801787fa6cfd268b6cb4ec5f0
Stephen Thompson <smt3@hotmail.com>
2019-07-27 14:04:14 -0500

    started coding API call

--- strainAPI/strainCall.js
+++ strainAPI/strainCall.js
@@ -1,2 +1,11 @@
 //Global Variables
 var apikey = 'dOdR7S1';
+var race = ['hybrid','sativa','indica'];
+
+
+//onclick API call
+$('#Bhang Granddaddy Purple').on('click', function(event){
+event.preventDefault();
+$('#displayInfo').empty();
+var queryURL = ""
+});
