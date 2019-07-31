51e40f84734e7eee5de4bc7a65f12b4605280565
joshroy326 <joshroy326@gmail.com>
2019-07-31 17:24:01 -0500

    made the function and ajax call when a pic is uploaded it will pull and return the attribute of the age

--- assets/javascript/faceApp.js
+++ assets/javascript/faceApp.js
@@ -1,19 +1,20 @@
-// alert("hi");
+function PreviewImage() {
+    var fileReader = new FileReader();
+    fileReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+    fileReader.onload = function (fileReaderEvent) {
+        $.ajax({
+            url: 'https://api-us.faceplusplus.com/facepp/v3/detect',
+            method: "POST",
+            data: {
+                api_key: '2Enaf2_TbEt5jqt3eRPTQ3VhgeE7FWx7',
+                api_secret: 'txBjyMf6ovNgpNxqdT56mi96YrvUpza_',
+                image_base64: fileReaderEvent.target.result,
+                return_attributes: 'emotion,gender,age',
+            }
+        }).then(function (response) {
+            console.log(response);
+        });
 
-
-
-
-
-$.ajax({
-    url: 'https://api-us.faceplusplus.com/facepp/v3/detect',
-    method: "POST",
-    data: {
-        api_key: '2Enaf2_TbEt5jqt3eRPTQ3VhgeE7FWx7',
-        api_secret: 'txBjyMf6ovNgpNxqdT56mi96YrvUpza_',
-        image_url: 'https://www.armytimes.com/resizer/X-5khefUIa4k4Wlzt_2hUn9q8Gc=/1200x0/filters:quality(100)/arc-anglerfish-arc2-prod-mco.s3.amazonaws.com/public/5LON7K6UGVFPTLMWZHOQBETMPI.jpg',
-        return_attributes: 'emotion,gender,age',
-    }
-}).then(function (response) {
-    console.log(response);
-});
+    };
+};
 
