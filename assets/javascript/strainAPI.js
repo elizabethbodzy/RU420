83f492150833ec637eddfb1746e9a3e87a4659b5
Stephen Thompson <smt3@hotmail.com>
2019-07-31 20:40:16 -0500

    added call to weed.js

--- assets/javascript/strainAPI.js
+++ assets/javascript/strainAPI.js
@@ -1,32 +1,36 @@
 //Global Variables
 
 //onclick API call
-function showEffects(){
-$('button').on('click', function(event){
-//event.preventDefault();
-$('#display-data').empty();
-var strainId = $(this).attr('strainID');
+function showEffects() {
+    $("button").on("click", function(event) {
+        //event.preventDefault();
+        $("#display-data").empty();
+        var strainId = $(this).attr("strainID");
 
-console.log(strainId);
-var effectsQueryURL = "http://strainapi.evanbusse.com/dOdR7S1/strains/data/effects/"+ strainId +"";
-$.ajax({ 
-    url: effectsQueryURL,
-    method: "GET"
-}).then(function(strainResponse){
-
-console.log(strainResponse.positive);
-var positiveReponse = strainResponse.positive;
-
-for(i = 0; i < positiveReponse.length; i++ ){
-    
-    var effectDivTag = $('<div>').addClass('effects');
-    var spanTag = $('<h4>').html(positiveReponse[i]).addClass('effectTitle');
-    effectDivTag.prepend(spanTag);
-    $('#display-data').prepend(effectDivTag);
-   // $(effectDivTag).appendTo(selector);
-console.log(positiveReponse[i]);
-};
-});
-});};
+        var effectsQueryURL =
+            "http://strainapi.evanbusse.com/dOdR7S1/strains/data/effects/" +
+            strainId +
+            "";
+        $.ajax({
+            url: effectsQueryURL,
+            method: "GET"
+        }).then(function(strainResponse) {
+            var positiveReponse = strainResponse.positive;
+            for (i = 0; i < positiveReponse.length; i++) {
+                var effectDivTag = $("<div>").addClass("effects");
+                var spanTag = $("<h4>")
+                    .html(positiveReponse[i])
+                    .addClass("effectTitle");
+                effectDivTag.prepend(spanTag);
+                $("#display-data").prepend(effectDivTag);
+                // $(effectDivTag).appendTo(selector);
+            
+            }
+            displayMoodInfo(positiveReponse);
+        });
+    });
+}
 
 showEffects();
+
+
