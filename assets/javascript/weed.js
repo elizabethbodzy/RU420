b74817f4c3b75257a247f5a72db49d45cfe99d95
mikedawn <biggrayson_2000@yahoo.com>
2019-08-03 12:40:00 -0500

    added categories

--- assets/javascript/weed.js
+++ assets/javascript/weed.js
@@ -1,39 +1,106 @@
-function displayMoodInfo(effects) {
+function displayPositive(effects) {
     console.log(effects);
     var networkRequests = [];
-
+​
     for (var i = 0; i < effects.length; i++) {
         var queryURL =
             "https://api.giphy.com/v1/gifs/search?q=" +
             effects[i] +
-            "&api_key=NboZIbbhYd3ktqKYAPpw3huD841MgC4E&limit=1";
+            "&api_key=NboZIbbhYd3ktqKYAPpw3huD841MgC4E&limit=5";
         var networkRequest = $.ajax({
             url: queryURL,
             method: "GET"
         });
-
+​
         networkRequests.push(networkRequest);
     }
-
     Promise.all(networkRequests).then(function(responses) {
-
          for (var i = 0; i < responses.length; i++) {
+             console.log({test: responses});
              var gifDiv = $("<div class='myEffect'>");
+​
             var spanTag = $('<span>').html(effects[i] + '<br>');
-             var imageUrl = responses[i].data[0].images.fixed_height_small.url;
-
+             var imageUrl = responses[i].data[0].images.fixed_width.url;
+​
              var imageTag = "<img src='" + imageUrl + "' alt=''>";
-
-             //gifDiv.append(imageTag);
-             //$(".effects").prepend(gifDiv);
-
+​
              gifDiv.prepend(spanTag);
              gifDiv.append(imageTag);
-             $('#display-data').prepend(gifDiv);
-             
+             $('#display-positive').append(gifDiv);
          }
-
     });
-    var effectMe = $(this).attr("");
-}
+};
+​
+function displayNegative(effects) {
+    console.log(effects);
+    var networkRequests = [];
+​
+    for (var i = 0; i < effects.length; i++) {
+        var queryURL =
+            "https://api.giphy.com/v1/gifs/search?q=" +
+            effects[i] +
+            "&api_key=NboZIbbhYd3ktqKYAPpw3huD841MgC4E&limit=5";
+        var networkRequest = $.ajax({
+            url: queryURL,
+            method: "GET"
+        });
+​
+        networkRequests.push(networkRequest);
+    }
+    Promise.all(networkRequests).then(function(responses) {
+         for (var i = 0; i < responses.length; i++) {
+             console.log({test: responses});
+             var gifDiv = $("<div class='myEffect'>");
+​
+            var spanTag = $('<span>').html(effects[i] + '<br>');
+             var imageUrl = responses[i].data[0].images.fixed_width.url;
+​
+             var imageTag = "<img src='" + imageUrl + "' alt=''>";
+​
+             gifDiv.prepend(spanTag);
+             gifDiv.append(imageTag);
+             $('#display-negative').append(gifDiv);
+         }
+    });
+};
+​
+function displayMedical(effects) {
+    console.log(effects);
+    var networkRequests = [];
+​
+    for (var i = 0; i < effects.length; i++) {
+        var queryURL =
+            "https://api.giphy.com/v1/gifs/search?q=" +
+            effects[i] +
+            "&api_key=NboZIbbhYd3ktqKYAPpw3huD841MgC4E&limit=5";
+        var networkRequest = $.ajax({
+            url: queryURL,
+            method: "GET"
+        });
+​
+        networkRequests.push(networkRequest);
+    }
+    Promise.all(networkRequests).then(function(responses) {
+         for (var i = 0; i < responses.length; i++) {
+             console.log({test: responses});
+             var gifDiv = $("<div class='myEffect'>");
+            
+            var spanTag = $('<span>').html(effects[i] + '<br>');
+             var imageUrl = responses[i].data[0].images.fixed_width.url;
+​
+             var imageTag = "<img src='" + imageUrl + "' alt=''>";
+​
+             gifDiv.prepend(spanTag);
+             gifDiv.append(imageTag);
+             
+             $('#display-medical').append(gifDiv);
+         }
+    });
+};
+Collapse
+
+
+
+
+
 //displayMoodInfo(strainResponse.positive);
