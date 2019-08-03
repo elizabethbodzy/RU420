0464a8acead3d33c1264abe78e4501bae550ada4
Stephen Thompson <smt3@hotmail.com>
2019-08-03 11:53:20 -0500

    removed for loops for html/css formatting

--- assets/javascript/strainAPI.js
+++ assets/javascript/strainAPI.js
@@ -1,5 +1,3 @@
-//Global Variables
-
 //onclick API call
 function showEffects() {
         $('#display-data').empty();
@@ -16,33 +14,7 @@ function showEffects() {
             var positiveResponse = strainResponse.positive;
             var negativeResponse = strainResponse.negative;
             var medicalResponse = strainResponse.medical;
-                for (i = 0; i < positiveResponse.length; i++) {
-                    var positiveEffectDivTag = $('<div>').addClass('positiveEffects');
-                    var spanPositiveTag = $('<h4>')
-                    .html(positiveResponse[i])
-                    .addClass('positiveEffectTitle');
-                    positiveEffectDivTag.prepend(spanPositiveTag);
-                    //console.log(strainResponse);
-                    };
 
-                for(i = 0; i < negativeResponse.length; i++ ){
-                    var negativeEffectDivTag = $('<div>').addClass('negativeEffects');
-                    var spanNegativeTag = $('<h4>')
-                    .html(negativeResponse[i])
-                    .addClass('negativeEffectTitle');
-                    negativeEffectDivTag.prepend(spanNegativeTag);
-                    //console.log(strainResponse);
-                    };
-
-                for(i = 0; i < medicalResponse.length; i++){
-                    var medicalEffectDivTag = $('<div>').addClass('medicalEffects');
-                    var spanMedicalTag = $('<h4>')
-                    .html(medicalResponse[i])
-                    .addClass('medicalEffectTitle');
-                    medicalEffectDivTag.prepend(spanMedicalTag);
-                    //console.log(strainResponse);
-                    };
-            
                 displayMoodInfo(positiveResponse);
                 displayMoodInfo(negativeResponse);
                 displayMoodInfo(medicalResponse);
